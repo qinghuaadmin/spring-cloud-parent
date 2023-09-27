a9b18b2a965703980848300a1f4072ea5d0d7cd8
kyle.hou <kyle.hou@meetsocial.com>
2023-09-27 20:37:33 +0800

    Initial commit

---
+++ src/test/java/com/openkeji/springcloudparent/SpringCloudParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openkeji.springcloudparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
